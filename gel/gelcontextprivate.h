a44ae4459542b9a39575675ffdec8e02a49bcf09
Sandino Flores <tigrux@gmail.com>
2011-01-12 18:06:40 -0600

    Move private definition of GelContext to its own header

---
+++ gel/gelcontextprivate.h
@@ -0,0 +1,16 @@
+#ifndef __GEL_CONTEXT_PRIVATE_H__
+#define __GEL_CONTEXT_PRIVATE_H__
+
+#include <gelcontext.h>
+
+
+struct _GelContext
+{
+    GHashTable *symbols;
+    GelContext *outer;
+    GList *closures;
+    volatile gint ref_count;
+};
+
+
+#endif
