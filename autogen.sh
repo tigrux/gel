c0f61249d2ecb953864edeb9d2df157cc17bfbd0
Sandino Flores <tigrux@gmail.com>
2011-08-06 01:20:25 -0500

    Make autogen.sh more descriptive

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,15 @@
-gtkdocize
-autoreconf -i
+#!/bin/sh
+if which gtkdocize; then
+    gtkdocize
+else
+    echo You must install gtk-doc-tools
+    exit 1
+fi
+
+if which autoreconf; then
+  autoreconf -i
+else
+    echo You must install autoconf
+    exit 1
+fi
+
