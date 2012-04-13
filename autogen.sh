151927e593af4a322707c7bf88daf412dbcd8b96
Sandino Flores <tigrux@gmail.com>
2012-04-12 22:55:20 -0500

    Update autogen.sh to provide more precise errors

--- autogen.sh
+++ autogen.sh
@@ -1,19 +1,42 @@
 #!/bin/sh
 
-if which gtkdocize; then
-    gtkdocize
-else
-    echo The package gtk-doc-tools is not installed
+missing() {
+    echo The package $1 is not installed
+}
+
+
+ERROR=no
+
+if ! which gtkdocize > /dev/null; then
+    missing gtk-doc-tools
+    ERROR=yes
+fi
+
+if ! which autoreconf > /dev/null; then
+    missing autoconf
+    ERROR=yes
+fi
+
+if ! pkg-config --exists gobject-introspection-1.0; then
+    missing libgirepository1.0-dev
+    ERROR=yes
+fi
+
+if ! pkg-config --exists gtk+-2.0; then
+    missing libgtk2.0-dev
+    ERROR=yes
+fi
+
+
+if test x$ERROR = xyes; then
     exit 1
 fi
 
-if which autoreconf; then
-    autoreconf -i
-else
-    echo The package autoreconf is not installed
-    exit 1
-fi
+
+gtkdocize
+autoreconf -i
 
 if test -f configure; then
-    ./configure $@
+    ./configure --enable-gtk-doc $@
 fi
+
