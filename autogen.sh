a1ed15b249f621bc3051a55460ee01729ce213ed
Sandino Flores <tigrux@gmail.com>
2012-04-15 20:22:33 -0500

    Make the autogen.sh suggest a command to install missing dependencies

--- autogen.sh
+++ autogen.sh
@@ -1,34 +1,28 @@
 #!/bin/sh
 
-missing() {
-    echo The package $1 is not installed
-}
-
-
-ERROR=no
+MISSING=""
 
 if ! which gtkdocize > /dev/null; then
-    missing gtk-doc-tools
-    ERROR=yes
+    MISSING="$MISSING gtk-doc-tools"
 fi
 
 if ! which autoreconf > /dev/null; then
-    missing autoconf
-    ERROR=yes
+    MISSING="$MISSING autoconf"
 fi
 
 if ! pkg-config --exists gobject-introspection-1.0; then
-    missing libgirepository1.0-dev
-    ERROR=yes
+    MISSING="$MISSING libgirepository1.0-dev"
 fi
 
 if ! pkg-config --exists gtk+-2.0; then
-    missing libgtk2.0-dev
-    ERROR=yes
+    MISSING="$MISSING libgtk2.0-dev"
 fi
 
 
-if test x$ERROR = xyes; then
+if test -n "$MISSING"; then
+    echo "Some required packages are missing"
+    echo "If you are using debian or ubuntu, install them with:"
+    echo "sudo apt-get install $MISSING"
     exit 1
 fi
 
