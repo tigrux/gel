165350db2c84cd2526b2625e35f91bfb524533b3
Sandino Flores <tigrux@gmail.com>
2012-05-01 09:49:23 -0500

    Convert from g_warning to g_propagate_error

--- libgel/geldebug.c
+++ libgel/geldebug.c
@@ -13,21 +13,27 @@ const gchar* plural(guint n)
 void gel_warning_needs_at_least_n_arguments(GelContext *context,
                                             const gchar *func, guint n)
 {
-    g_warning("%s: Needs at least %u argument%s", func, n, plural(n));
+    gel_context_set_error(context, g_error_new(
+        GEL_CONTEXT_ERROR, GEL_CONTEXT_ERROR_ARGUMENTS,
+        "%s: Needs at least %u argument%s", func, n, plural(n)));
 }
 
 
 void gel_warning_needs_n_arguments(GelContext *context,
                                    const gchar *func, guint n)
 {
-    g_warning("%s: Needs %u argument%s", func, n, plural(n));
+    gel_context_set_error(context, g_error_new(
+        GEL_CONTEXT_ERROR, GEL_CONTEXT_ERROR_ARGUMENTS,
+        "%s: Needs %u argument%s", func, n, plural(n)));
 }
 
 
 void gel_warning_no_such_property(GelContext *context,
                                   const gchar *func, const gchar *prop_name)
 {
-    g_warning("%s: No such property '%s'", func, prop_name);
+    gel_context_set_error(context, g_error_new(
+        GEL_CONTEXT_ERROR, GEL_CONTEXT_ERROR_PROPERTY,
+        "%s: No such property '%s'", func, prop_name));
 }
 
 
@@ -36,8 +42,10 @@ void gel_warning_value_not_of_type(GelContext *context, const gchar *func,
 {
     gchar *value_string = gel_value_to_string(value);
 
-    g_warning("%s: '%s' is not of type '%s'", func,
-        value_string, g_type_name(type));
+    gel_context_set_error(context, g_error_new(
+        GEL_CONTEXT_ERROR, GEL_CONTEXT_ERROR_TYPE,
+        "%s: '%s' is not of type '%s'", func,
+        value_string, g_type_name(type)));
 
     g_free(value_string);
 }
@@ -46,14 +54,18 @@ void gel_warning_value_not_of_type(GelContext *context, const gchar *func,
 void gel_warning_unknown_symbol(GelContext *context,
                                 const gchar *func, const gchar *symbol)
 {
-    g_warning("%s: Unknown symbol '%s'", func, symbol);
+    gel_context_set_error(context, g_error_new(
+        GEL_CONTEXT_ERROR, GEL_CONTEXT_ERROR_SYMBOL,
+        "%s: Unknown symbol '%s'", func, symbol));
 }
 
 
 void gel_warning_type_not_instantiatable(GelContext *context,
                                          const gchar *func, GType type)
 {
-    g_warning("%s: Type %s is not instantiatable", func, g_type_name(type));
+    gel_context_set_error(context, g_error_new(
+        GEL_CONTEXT_ERROR, GEL_CONTEXT_ERROR_TYPE,
+        "%s: Type %s is not instantiatable", func, g_type_name(type)));
 }
 
 
@@ -64,9 +76,11 @@ void gel_warning_invalid_value_for_property(GelContext *context,
 {
     gchar *value_string = gel_value_to_string(value);
 
-    g_warning("%s: '%s' of type '%s' is invalid for property '%s' of type '%s'",
+    gel_context_set_error(context, g_error_new(
+        GEL_CONTEXT_ERROR, GEL_CONTEXT_ERROR_PROPERTY,
+        "%s: '%s' of type '%s' is invalid for property '%s' of type '%s'",
         func, value_string, GEL_VALUE_TYPE_NAME(value),
-        pspec->name, g_type_name(pspec->value_type));
+        pspec->name, g_type_name(pspec->value_type)));
 
     g_free(value_string);
 }
@@ -75,28 +89,37 @@ void gel_warning_invalid_value_for_property(GelContext *context,
 void gel_warning_type_name_invalid(GelContext *context,
                                    const gchar *func, const gchar *name)
 {
-    g_warning("%s: '%s' is not a registered type", func, name);
+
+    gel_context_set_error(context, g_error_new(
+        GEL_CONTEXT_ERROR, GEL_CONTEXT_ERROR_TYPE,
+        "%s: '%s' is not a registered type", func, name));
 }
 
 
 void gel_warning_invalid_argument_name(GelContext *context,
                                        const gchar *func, const gchar *name)
 {
-    g_warning("%s: '%s' is an invalid argument name", func, name);
+    gel_context_set_error(context, g_error_new(
+        GEL_CONTEXT_ERROR, GEL_CONTEXT_ERROR_ARGUMENTS,
+        "%s: '%s' is an invalid argument name", func, name));
 }
 
 
 void gel_warning_index_out_of_bounds(GelContext *context,
                                      const gchar *func)
 {
-    g_warning("%s: Index out of bounds", func);
+    gel_context_set_error(context, g_error_new(
+        GEL_CONTEXT_ERROR, GEL_CONTEXT_ERROR_INDEX,
+        "%s: Index out of bounds", func));
 }
 
 
 void gel_warning_symbol_exists(GelContext *context,
                                const gchar *func, const gchar *name)
 {
-    g_warning("%s: Symbol '%s' already exists", func, name);
+    gel_context_set_error(context, g_error_new(
+        GEL_CONTEXT_ERROR, GEL_CONTEXT_ERROR_SYMBOL,
+        "%s: Symbol '%s' already exists", func, name));
 }
 
 
@@ -104,7 +127,7 @@ void gel_warning_expected(GelContext *context,
                           const gchar *func, const gchar *s)
 {
     gel_context_set_error(context, g_error_new(
-        GEL_CONTEXT_ERROR, GEL_CONTEXT_ERROR_EXPECTED,
+        GEL_CONTEXT_ERROR, GEL_CONTEXT_ERROR_ARGUMENTS,
         "%s: Expected %s", func, s));
 }
 
